cb2babacaaac3e82b3d6bf6c69cbe51a77b2cc60
Klaudia Lenarcik <klalen438@gmail.com>
2023-08-02 21:11:17 +0200

    Adding project to repository

---
+++ Books/Controllers/CategoryController.cs
@@ -0,0 +1,111 @@
+﻿using Books.Data;
+using Books.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Books.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+		public IActionResult Create(Category obj)
+		{
+            if (ModelState.IsValid)
+            {
+                if (obj.Name == obj.DisplayOrder.ToString())
+                {
+                    ModelState.AddModelError("", "Name and Display Order must have different values");
+                }
+                else
+                {
+					TempData["success"] = "Category created successfully";
+                    _db.Categories.Add(obj);
+                    _db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+			}
+			 return View(obj);
+		}
+		//GET
+		public IActionResult Edit(int? Id)
+		{
+            if (Id == 0 || Id == null)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(Id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return View(categoryFromDb);
+            }
+		}
+		//POST
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Edit(Category obj)
+		{
+			if (ModelState.IsValid)
+			{
+				if (obj.Name == obj.DisplayOrder.ToString())
+				{
+					ModelState.AddModelError("", "Name and Display Order must have different values");
+				}
+				else
+				{
+					TempData["success"] = "Category edited successfully";
+					_db.Categories.Update(obj);
+					_db.SaveChanges();
+					return RedirectToAction("Index");
+				}
+			}
+			return View(obj);
+		}
+		//GET
+		public IActionResult Delete(int? Id)
+		{
+			if (Id == 0 || Id == null)
+			{
+				return NotFound();
+			}
+			var categoryFromDb = _db.Categories.Find(Id);
+			if (categoryFromDb == null)
+			{
+				return NotFound();
+			}
+			else
+			{
+				return View(categoryFromDb);
+			}
+		}
+		//POST
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Delete(Category obj)
+		{
+			TempData["success"] = "Category deleted successfully";
+			_db.Categories.Remove(obj);
+			_db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+	}
+}
---
+++ Books/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Books.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage = "Value must be betwen 1 and 100")]
+        public string DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
